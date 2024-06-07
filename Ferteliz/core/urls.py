02d9a657cd6582751fb42ba31b6c755094024e60
Lucas Arantes <108706950+Arantees@users.noreply.github.com>
2024-06-07 15:10:55 -0300

    in progress conection with mongoDB

--- Ferteliz/core/urls.py
+++ Ferteliz/core/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import register
+from .views import add_product, list_products ,register
 
 urlpatterns = [
     path('register/', register, name='register'),
+    # path('food_items/', name='list_food_items'),
+    # path('add_food_item/', name='add_food_item'),
+    path('add_product/', add_product, name='add_product'),
+    path('list_products/', list_products, name='list_products'),
 ]
