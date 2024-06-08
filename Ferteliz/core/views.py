54c607e6caf51e85988998f88033f525c0a7378a
Lucas Luiz Assis <83933766+Luhcyy@users.noreply.github.com>
2024-06-08 12:28:25 -0300

    update core views file

--- Ferteliz/core/views.py
+++ Ferteliz/core/views.py
@@ -1,63 +1,18 @@
-from django.http import JsonResponse
-from django.shortcuts import render, redirect, HttpResponseRedirect, get_object_or_404
-from django.contrib.auth import login
-from django.urls import reverse
-from Ferteliz import settings
-from core.models import UserModel, ProductModel
-from .forms import UserForm, ProductForm
 
-def home(request):
-    if request.method == 'GET':
-        return render(request, 'home.html')
-    return HttpResponseRedirect(reverse('home'))
+from django.shortcuts import render
 
-def register(request):
-    print(request)
-    if request.method == 'POST':
-        form = UserForm(request.POST)
-        if form.is_valid():
-            UserModel.objects.create(**form.cleaned_data)
-            user = form.save()
-            login(request, user, backend='django.contrib.auth.backends.ModelBackend')
-            return redirect(reverse('home'))
-    else:
-        form = UserForm()
-        contexto = {'form': form}
-        return render(request, 'register.html', contexto)
+# Create your views here.
+def home (request):
+    return render(request, 'home.html')
 
-def list_products(request):
-    products = ProductModel.objects.all()
-    data = []
-    for product in products:
-        data.append({
-            'name': product.name,
-            'description': product.description,
-            'price': str(product.price)  # Convertendo para string para garantir compatibilidade com JSON
-        })
-    return JsonResponse(data, safe=False)
+def cadastroMenu (request):
+    return render(request, 'cadastroMenu.html')
 
-def add_product(request):
-    if request.method == 'POST':
-        form = ProductForm(request.POST, request.FILES)
-        if form.is_valid():
-            # Se salvar no Django ORM:
-            #   form.save()
-            #   return HttpResponseRedirect(reverse('list_products'))
-            # else:
-            #   form = ProductForm()
+def login (request):
+    return render(request, 'login.html')
 
-            # Salvando diretamente no MongoDB:
-            data = form.cleaned_data
-            product = {
-                'name': data.get('name'),
-                'description': data.get('description'),
-                'price': float(data.get('price'))
-            }
-            db = settings.db
-            db.products.insert_one(product)
-            return JsonResponse({'status': 'Product added successfully'})
-        else:
-            return JsonResponse({'status': 'Form is not valid'}, status=400)
-    else:
-        form = ProductForm()
-    return render(request, 'add_product.html', {'form': form})
+def cadastroCliente (request):
+    return render(request, 'cadastroCliente.html')
+
+def cadastroVendedor (request):
+    return render(request, 'cadastroVendedor.html')
